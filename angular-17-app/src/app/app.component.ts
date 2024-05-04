ae02d98df70b20b6da1aa813d5a1ed058357df92
Julio Mendoza <angel.66.mendoza@gmail.com>
2024-05-03 18:21:36 -0600

    Cambio menor en titulo de app

--- angular-17-app/src/app/app.component.ts
+++ angular-17-app/src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Título de aplicación de Angular';
+  title = 'Título de aplicación de Angular verison 17';
 }
