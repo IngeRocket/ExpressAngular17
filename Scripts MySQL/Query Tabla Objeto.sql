1f0ff95f902ae31b42535b4386861ad92ee05e6a
Julio Mendoza <angel.66.mendoza@gmail.com>
2024-05-11 16:31:08 -0600

    Integracion de MySQL scripts y elementos nuevos

---
+++ Scripts MySQL/Query Tabla Objeto.sql
@@ -0,0 +1,13 @@
+#Este script es unicamente de prueba para comprobar que la aplicación se puede conectar de forma correcta con el servicio de MySQL Workbench
+/*
+create database prueba;
+use prueba;
+delimiter ;;
+create table objeto(
+objeto_id int auto_increment primary key,
+objeto_nombre varchar(50) not null,
+objeto_ruta varchar(50) not null,
+objeto_extension varchar(6) not null
+);
+;;
+*/
